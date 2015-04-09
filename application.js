725c82f790d3b47a4290bad958bd520afec8d67e
JulianOrinyol <julianorinyol@gmail.com>
2015-04-08 17:26:57 -0700

    added to js file, creating html buttons for search results

--- application.js
+++ application.js
@@ -3,29 +3,39 @@ console.log('doc ready')
 
 
 cities = [];
-console.log(cities)
+var city_objs = [];
+
+
+var sortCities = function(cityObj){
+  cityObj.forEach(function(city) {
+     console.log(city);
+
+     $('body').append( "<br><button>" + city.name +"</button>" );
+   // city_objs.RESULTS[0].name
+   });
+
+
+
+}
+
   $('#submit-button').on('click', function(e){
     e.preventDefault;
 
     console.log('clicked');
     var query = $('#city-search-field').val();
-    console.log('search = ' + query);
+    // console.log('search = ' + query);
 
-    city_objs = '';
     $.ajax({
       method: "GET",
       jsonp: 'cb',
       dataType: "jsonp",
       url: "http://autocomplete.wunderground.com/aq?query=" + query,
       success: function(response){
-        console.log(response);
-        city_objs = response;
+        // console.log(response);
+       sortCities(response.RESULTS);
       }
     });
-  //   city_objs.forEach(function(
-  //     console.log(city)
-  //     // city_objs.RESULTS[0].name
-  //   ), city)
+
   });
 
 
