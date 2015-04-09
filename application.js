6d6ef4789b80954db11d486b6b98405bb972b934
JulianOrinyol <julianorinyol@gmail.com>
2015-04-08 18:18:35 -0700

    Got the weather search ajax working

--- application.js
+++ application.js
@@ -1,13 +1,25 @@
 $(function(){
   console.log('doc ready')
 
-  var counter = 0;
+  var cities = [];
 
   var sortCities = function(cityObj){
     cityObj.forEach(function(city) {
-       $('body').append( "<br><button id='" + counter + "'>" + city.name +"</button>");
-      counter++;
+       $('#results').append( "<br><button>" + city.name +"</button>");
      });
+  }
+
+  var getWeather = function(id) {
+    $.ajax({
+      method: "GET",
+      // jsonp: 'cb',
+      dataType: "jsonp",
+      // url: "http://api.wunderground.com/api/4c2c6e229659e196/forecast/q/zmw:" + id + ".json",
+      url: "http://api.wunderground.com/api/6cc5df2c6acc5d95/forecast/q/zmw:" + id + ".json",
+      success: function(response){
+        console.log('success')
+      }
+    });
   }
 
   $('#submit-button').on('click', function(e){
@@ -23,14 +35,24 @@ $(function(){
       dataType: "jsonp",
       url: "http://autocomplete.wunderground.com/aq?query=" + query,
       success: function(response){
-        // console.log(response);
        sortCities(response.RESULTS);
+       cities = response.RESULTS;
       }
     });
-
   });
 
-  $('')
+  $('#results').on('click', 'button', function(e){
+    var cityName = $(this).text()
+    cities.forEach(function(city) {
+      if(city.name === cityName) {
+        getWeather(city.zmw);
+      }
+    });
+    // $(this).attr('id')
+  });
+  
+  
+
 
 
 });
